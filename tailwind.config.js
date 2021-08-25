2297f8db2ebd94d8137155104a1ea708012b7d09
evelynfredin <evefre2001@skola.goteborg.se>
2021-08-25 16:22:00 +0200

    Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: "jit",
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Lota Grotesque", ...fontFamily.sans],
+            },
+            colors: {
+                rdorange: "#e07c3e",
+                rdgreen: "#009392",
+                rdyellow: "#f6cd2d",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
