9c784b1a6124d3bfe90e87c19c6786855b0b4134
evelynfredin <evefre2001@skola.goteborg.se>
2021-08-27 11:09:31 +0200

    Added missing member picture

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="px-5 md:px-0 mb-10 mt-32 container mx-auto flex justify-between">
+        <div className="px-5 md:px-0 mb-10 mt-36 items-center container mx-auto flex md:justify-between md:items-center flex-col md:flex-row">
             <div>
                 <a href="http://yrgo.se">
                     <Image
@@ -16,7 +16,6 @@ const Footer = () => {
                         className="hover:opacity-80"
                     />
                 </a>
-                <p className="text-sm text-gray-400">Copyright © 2021</p>
             </div>
             <div>
                 <a href="https://raddningsmissionen.se/">
